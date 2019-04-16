ce0a01bb2a721e0fc34b866a864e932b2160cc6d
damokravetz <kravetzdamian@gmail.com>
2019-04-15 22:47:59 -0300

    ej3 testv1

--- tp2/src/edu/ort/taller/ejer3/Test.java
+++ tp2/src/edu/ort/taller/ejer3/Test.java
@@ -1,9 +1,14 @@
 package edu.ort.taller.ejer3;
 
+import java.util.ArrayList;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
+		ArrayList <Electrodomestico>productos=new ArrayList<Electrodomestico>();
+		Heladera a=new Heladera("Whirlpool", "H2745", 123456, 220, false, 14999, 250, true);
+		Lavarropa b=new Lavarropa("Phillips", "49PGFS", 123456, 220, false, 14370, 49, true);
 
 	}
 
